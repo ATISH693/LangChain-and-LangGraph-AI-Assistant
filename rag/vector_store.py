9e29a595f8e0ece2b27c3ac5011da84375ced212
ATISH693 <atishsawant693@gmail.com>
2026-07-17 19:42:55 +0530

    Use CPU-only PyTorch

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -13,10 +13,14 @@ def get_vector_store():
     """
 
     vector_store = PGVector(
-        embeddings=get_embedding_model(),
-        collection_name="langgraph_docs",
-        connection=os.getenv("DATABASE_URL"),
-        use_jsonb=True,
+    embeddings=get_embedding_model(),
+    collection_name="langgraph_docs",
+    connection=os.getenv("DATABASE_URL"),
+    use_jsonb=True,
+    engine_args={
+        "connect_args": {
+            "prepare_threshold": None
+            }
+        }
     )
-
     return vector_store
